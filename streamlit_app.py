44c36e2aba9489fc4f6b6de12ecae4e6bb3d261d
JOHNSEENA <rishikeshanand51@gmail.com>
2026-05-20 04:38:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ st.markdown("""
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
 * { font-family: 'Poppins', sans-serif; }
 .stApp {
-    background-image: url('https://i.postimg.cc/TYhXd0gG/d0a72a8cea5ae4978b21e04a74f0b0ee.jpg');
+    background-image: url('https://i.imgur.com/TvlTdZO.jpeg');
     background-size: cover; background-position: center; background-attachment: fixed;
 }
 .main .block-container {
